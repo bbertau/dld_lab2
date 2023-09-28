de06d4ce9208edf1737453af6853856e0be3a6be
Bertaux <brent.bertaux@okstate.edu>
2023-09-28 17:15:06 -0500

    fun code

--- Lab2/SV/des_stub.sv
+++ Lab2/SV/des_stub.sv
@@ -37,6 +37,65 @@ module PC1 (key, left_block, right_block);
    output logic [27:0] right_block;
 
    logic [55:0]        out_block;
+  assign left_block[0] = key[64-36];
+   assign left_block[1] = key[64-44];
+   assign left_block[2] = key[64-52];
+   assign left_block[3] = key[64-60];
+  assign left_block[4] = key[64-3];
+  assign left_block[5] = key[64-11];
+  assign left_block[6] = key[64-19];
+  assign left_block[7] = key[64-27];
+  assign left_block[8] = key[64-35];
+  assign left_block[9] = key[64-43];
+  assign left_block[10] = key[64-51];
+ assign  left_block[11] = key[64-59];
+  assign left_block[12] = key[64-2];
+  assign left_block[13] = key[64-10];
+ assign  left_block[14] = key[64-18];
+ assign  left_block[15] = key[64-26];
+ assign  left_block[16] = key[64-34];
+  assign left_block[17] = key[64-42];
+ assign  left_block[18] = key[64-50];
+ assign  left_block[19] = key[64-58];
+ assign  left_block[20] = key[64-1];
+ assign  left_block[21] = key[64-9];
+ assign  left_block[22] = key[64-17];
+ assign  left_block[23] = key[64-25];
+ assign  left_block[24] = key[64-33];
+ assign left_block[25] = key[64-41];
+ assign  left_block[26] = key[64-49];
+ assign  left_block[27] = key[64-57];
+ 
+assign	right_block[0] = key[64-4];
+  assign right_block[1] = key[64-12];
+ assign  right_block[2] = key[64-20];
+ assign  right_block[3] = key[64-28];
+ assign  right_block[4] = key[64-5];
+assign   right_block[5] = key[64-13];
+ assign   right_block[6] = key[64-21];
+ assign  right_block[7] = key[64-29];
+ assign   right_block[8] = key[64-37];
+ assign  right_block[9] = key[64-45];
+ assign   right_block[10] = key[64-53];
+ assign   right_block[11] = key[64-61];
+ assign  right_block[12] = key[64-6];
+ assign   right_block[13] = key[64-14];
+ assign   right_block[14] = key[64-22];
+ assign   right_block[15] = key[64-30];
+ assign   right_block[16] = key[64-38];
+assign    right_block[17] = key[64-46];
+ assign   right_block[18] = key[64-54];
+ assign   right_block[19] = key[64-62];
+ assign   right_block[20] = key[64-7];
+ assign   right_block[21] = key[64-15];
+ assign  right_block[22] = key[64-23];
+ assign   right_block[23] = key[64-31];
+ assign   right_block[24] = key[64-39];
+ assign   right_block[25] = key[64-47];
+ assign   right_block[26] = key[64-55];
+assign    right_block[27] = key[64-63];
+
+	
 
 endmodule // PC1
 
@@ -45,6 +104,55 @@ module PC2 (left_block, right_block, subkey);
    input logic [27:0] left_block;
    input logic [27:0] right_block;
    output logic [47:0] subkey;
+   assign combined = {left_block, right_block};
+   assign subkey[47] = combined[48-14];
+   assign subkey[46] = combined[48-17];
+   assign subkey[45] = combined[48-11];
+   assign subkey[44] = combined[48-24];
+   assign subkey[43] = combined[48-1];
+   assign subkey[42] = combined[48-5];
+   assign subkey[41] = combined[48-3];
+   assign subkey[40] = combined[48-28];
+   assign subkey[39] = combined[48-15];
+   assign subkey[38] = combined[48-6];
+   assign subkey[37] = combined[48-21];
+   assign subkey[36] = combined[48-10];
+   assign subkey[35] = combined[48-23];
+   assign subkey[34] = combined[48-19];
+   assign subkey[33] = combined[48-12];
+   assign subkey[32] = combined[48-4];
+   assign subkey[31] = combined[48-26];
+   assign subkey[30] = combined[48-8];
+   assign subkey[29] = combined[48-16];
+   assign subkey[28] = combined[48-7];
+   assign subkey[27] = combined[48-27];
+   assign subkey[26] = combined[48-20];
+   assign subkey[25] = combined[48-13];
+   assign subkey[24] = combined[48-2];
+   assign subkey[23] = combined[48-41];
+   assign subkey[22] = combined[48-52];
+   assign subkey[21] = combined[48-31];
+   assign subkey[20] = combined[48-37];
+assign subkey[19] = combined[48-47];
+assign subkey[18] = combined[48-55];
+assign subkey[17] = combined[48-30];
+assign subkey[16] = combined[48-40];
+assign subkey[15] = combined[48-51];
+assign subkey[14] = combined[48-45];
+assign subkey[13] = combined[48-33];
+assign subkey[12] = combined[48-48];
+assign subkey[11] = combined[48-44];
+assign subkey[10] = combined[48-49];
+assign subkey[9] = combined[48-39];
+assign subkey[8] = combined[48-56];
+assign subkey[7] = combined[48-34];
+assign subkey[6] = combined[48-53];
+assign subkey[5] = combined[48-46];
+assign subkey[4] = combined[48-42];
+assign subkey[3] = combined[48-50];
+assign subkey[2] = combined[48-36];
+assign subkey[1] = combined[48-29];
+assign subkey[0] = combined[48-32];
 
 endmodule // PC2
 
@@ -62,6 +170,56 @@ module EF (inp_block, out_block);
    input logic [31:0] inp_block;
    output logic [47:0] out_block;
 
+   assign out_block[47] = inp_block[32 - 32];
+   assign out_block[46] = inp_block[32 - 1];
+   assign out_block[45] = inp_block[32 - 2];
+   assign out_block[44] = inp_block[32 - 3];
+   assign out_block[43] = inp_block[32 - 4];
+   assign out_block[42] = inp_block[32 - 5];
+   assign out_block[41] = inp_block[32 - 4];
+   assign out_block[40] = inp_block[32 - 5];
+   assign out_block[39] = inp_block[32 - 6];
+   assign out_block[38] = inp_block[32 - 7];
+   assign out_block[37] = inp_block[32 - 8];
+   assign out_block[36] = inp_block[32 - 9];
+   assign out_block[35] = inp_block[32 - 8];
+   assign out_block[34] = inp_block[32 - 9];
+   assign out_block[33] = inp_block[32 - 10];
+   assign out_block[32] = inp_block[32 - 11];
+   assign out_block[31] = inp_block[32 - 12];
+   assign out_block[30] = inp_block[32 - 13];
+   assign out_block[29] = inp_block[32 - 12];
+   assign out_block[28] = inp_block[32 - 13];
+   assign out_block[27] = inp_block[32 - 14];
+   assign out_block[26] = inp_block[32 - 15];
+   assign out_block[25] = inp_block[32 - 16];
+   assign out_block[24] = inp_block[32 - 17];
+   assign out_block[23] = inp_block[32 - 16];
+   assign out_block[22] = inp_block[32 - 17];
+   assign out_block[21] = inp_block[32 - 18];
+   assign out_block[20] = inp_block[32 - 19];
+   assign out_block[19] = inp_block[32 - 20];
+   assign out_block[18] = inp_block[32 - 21];
+   assign out_block[17] = inp_block[32 - 20];
+   assign out_block[16] = inp_block[32 - 21];
+   assign out_block[15] = inp_block[32 - 22];
+   assign out_block[14] = inp_block[32 - 23];
+   assign out_block[13] = inp_block[32 - 24];
+   assign out_block[12] = inp_block[32 - 25];
+   assign out_block[11] = inp_block[32 - 24];
+   assign out_block[10] = inp_block[32 - 25];
+   assign out_block[9] = inp_block[32 - 26];
+   assign out_block[8] = inp_block[32 - 27];
+   assign out_block[7] = inp_block[32 - 28];
+   assign out_block[6] = inp_block[32 - 29];
+   assign out_block[5] = inp_block[32 - 28];
+   assign out_block[4] = inp_block[32 - 29];
+   assign out_block[3] = inp_block[32 - 30];
+   assign out_block[2] = inp_block[32 - 31];
+   assign out_block[1] = inp_block[32 - 32];
+   assign out_block[0] = inp_block[32 - 1];
+
+
 endmodule // EF
 
 module feistel (inp_block, subkey, out_block);
@@ -78,6 +236,22 @@ module round (inp_block, subkey, out_block);
    input logic [63:0]  inp_block;
    input logic [47:0]  subkey;
    output logic [63:0] out_block;
+   logic [31:0] left_block;
+    logic [31:0] right_block;
+	logic [31:0] feistel_out;
+	logic [31:0] right_out;
+	logic [31:0] left_out;
+
+	assign {left_block, right_block} = inp_block;
+	feistel right_block(
+		.inp_block(right_block),
+		.out_block(feistel_out);
+		.subkey(subkey);
+	);
+
+	assign right_out[31:0] = (feistel_out) ^ left_block;
+	assign left_out = right_block; 
+	assign out_block = {left_block, right_block};
 
 endmodule // round1
 

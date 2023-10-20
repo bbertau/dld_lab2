7c75840b3a1665ad7b1b0b53bf0f5307721e5282
bbertau <143755539+bbertau@users.noreply.github.com>
2023-10-20 10:22:25 -0500

    Add files via upload

--- Lab2/top_demo.sv
+++ Lab2/top_demo.sv
@@ -40,74 +40,72 @@ module top_demo
   output logic [3:0] sseg_an
 );
 
-  logic [16:0] CURRENT_COUNT;
-  logic [16:0] NEXT_COUNT;
-  logic        smol_clk;
-  logic [15:0] toSeg;
+  logic [63:0] Key = 64'h133457799bbcdff1;
+  logic [63:0] IV = 64'h0000000000000001;
   logic encrypt;
   logic cipher;
+  logic clk;
+  logic [15:0] digit;
+  logic [3:0] disp = sw[3:0];
+  logic [16:0] c_clock;
+  logic [16:0] n_count;
+  logic [63:0] plaintext = 64'h123456abcd132536;
+  logic [63:0] ciphertext;
   assign encrypt = sw[7];
   assign cipher = sw[6];
-  
-  
-logic [64:0] plainText; 
-   always @(*) 
-   begin
-    case(sw[3:0])
-        4'b0000: toSeg = plainText[15:0];
-        4'b0001: toSeg = plainText[31:16];
-        4'b0010: toSeg = plainText[47:32];
-        4'b0011: toSeg = plainText[63:48];
-        default: toSeg = 16'h0;
+  assign led[0] = encrypt;
+  assign led[1] = cipher;
+  assign led[7]= (ciphertext == 64'hf77bcd7dfe57e119);
+  // Place DES instantiation here
+  DES dut(Key, plaintext, encrypt, cipher, IV, ciphertext);
+  always @ (*) begin
+    case (disp)
+    4'b0100: digit = Key[63:48];
+    4'b0101: digit = Key[47:32];
+    4'b0110: digit = Key[31:16];
+    4'b0111: digit = Key[15:0];
+    4'b1100: digit = IV[63:48];
+    4'b1101: digit = IV[47:32];
+    4'b1110: digit = IV[31:16];
+    4'b1111: digit = IV[15:0];
+    4'b0000: digit = plaintext[63:48];
+    4'b0001: digit = plaintext[47:32];
+    4'b0010: digit = plaintext[31:16];
+    4'b0011: digit = plaintext[15:0];
+    4'b1000: digit = ciphertext[63:48];
+    4'b1001: digit = ciphertext[47:32];
+    4'b1010: digit = ciphertext[31:16];
+    4'b1011: digit = ciphertext[15:0];
     endcase
-    end
-    
-    logic keyCheck;
-    logic [7:0] parityKeys;
-    logic [63:0] keys;
-    assign parityKeys[0] = ^keys[7:0];
-    assign parityKeys[1] = ^keys[15:8];
-    assign parityKeys[2] = ^keys[23:16];
-    assign parityKeys[3] = ^keys[31:24];
-    assign parityKeys[4] = ^keys[39:32];
-    assign parityKeys[5] = ^keys[47:40];
-    assign parityKeys[6] = ^keys[55:48];
-    assign parityKeys[7] = ^keys[63:56];
-    
-    assign keyCheck = ^parityKeys[7:0];
-    assign keyCheck = led[0];
-       
-    
+  end
   
   
-  // Place TicTacToe instantiation here
   
   // 7-segment display
   segment_driver driver(
-  .clk(smol_clk),
+  .clk(clk),
   .rst(btn[3]),
-  .digit0(sw[3:0]),
-  .digit1(4'b0111),
-  .digit2(sw[7:4]),
-  .digit3(4'b1111),
+  .digit0(digit[3:0]),
+  .digit1(digit[7:4]),
+  .digit2(digit[11:8]),
+  .digit3(digit[15:12]),
   .decimals({1'b0, btn[2:0]}),
   .segment_cathodes({sseg_dp, sseg_cg, sseg_cf, sseg_ce, sseg_cd, sseg_cc, sseg_cb, sseg_ca}),
   .digit_anodes(sseg_an)
   );
-  
+
 // Register logic storing clock counts
   always@(posedge sysclk_125mhz)
   begin
     if(btn[3])
-      CURRENT_COUNT = 17'h00000;
+      c_clock = 17'h00000;
     else
-      CURRENT_COUNT = NEXT_COUNT;
+      c_clock = n_count;
   end
-  
-  // Increment logic
-  assign NEXT_COUNT = CURRENT_COUNT == 17'd100000 ? 17'h00000 : CURRENT_COUNT + 1;
 
-  // Creation of smaller clock signal from counters
-  assign smol_clk = CURRENT_COUNT == 17'd100000 ? 1'b1 : 1'b0;
+  assign n_count = c_clock == 17'd100000 ? 17'h00000 : c_clock + 1;
+
+  assign clk = c_clock == 17'd100000 ? 1'b1 : 1'b0;
+
 
 endmodule
